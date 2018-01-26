f30b7a23ee51b36581e9ef776e0316ef43e51404
mike <dinhmq1@gmail.com>
2018-01-25 19:27:58 -0600

    add constructor to square class to init state

--- js/Game.jsx
+++ js/Game.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
       <button className="square" onClick={() => alert('Clicked!')}>
